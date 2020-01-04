f5a984a113d20b3afebeb77c68a538f5f9de5a14
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-04 12:40:46 +0200

    l5 task1 complete

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> skaiciai = new List<int>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                skaiciai.Add(rnd.Next(1,100));
+            }
+            Console.WriteLine("Sukurtas listas:");
+            skaiciai.ForEach(Console.WriteLine);
+            Console.ReadLine();
+
+            Console.WriteLine("Lyginiai listo elementai:");
+            skaiciai.ForEach(item =>
+            {
+                int s = Convert.ToInt32(item);
+                if (s%2 == 0)
+                {
+                    Console.WriteLine(s);
+                }
+            });
+            Console.ReadLine();
+        }
+    }
+}
